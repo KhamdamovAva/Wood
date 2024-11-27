1ef27244a16a121c1a9b13fd85a77a0e2c8c1394
KhamdamovAva <73744057+Number1ava@users.noreply.github.com>
2024-11-27 15:51:24 +0500

    finished navbar and changed padding in Home.jsx

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import ambarIcon from '../../assets/icons/ambarIcon.svg'
+import OrangeBtn from '../buttons/OrangeBtn/OrangeBtn'
 function Navbar() {
   return (
-    <nav className='flex items-center justify-between text-[14px] font-medium'>
-      <img src={ambarIcon} alt="Logotype" />
+    <nav className='flex items-center justify-between text-[14px] font-medium mb-[26px]'>
+      <a href="#"><img src={ambarIcon} alt="Logotype" /></a>
       <ul className='flex space-x-[44px]'>
         <li><a href="#">Каталог</a></li>
         <li><a href="#">Об амбарной доске</a></li>
@@ -12,9 +13,9 @@ function Navbar() {
         <li><a href="#">Блог</a></li>
         <li><a href="#">Контакты</a></li>
       </ul>
-      <div className='flex '>
-        <a href="#" className='w-[172px] text-[12px] block'>+7 (929) 590 82-87<br></br> Москва и Санкт-Петербург</a>
-        <button>Консультация</button>
+      <div className='flex items-center space-x-6'>
+        <a href="#" className='w-[172px] text-[12px] block'>+7 (929) 590 82-87 <span className='block text-[#C0C0C0]'>Москва и Санкт-Петербург</span></a>
+        <OrangeBtn>Консультация</OrangeBtn>
       </div>
     </nav>
   )
