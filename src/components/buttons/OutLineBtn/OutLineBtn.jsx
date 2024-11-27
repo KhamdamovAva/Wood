d074e128d9600fb0807dfdac815fc3cddae3dbf5
Beksulton10 <beksulton460@gmail.com>
2024-11-27 15:32:58 +0500

    I did some changes

---
+++ src/components/buttons/OutLineBtn/OutLineBtn.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function OutLineBtn({ children }) {
+  return (
+    <div className='border-2 border-[#D8A47A] py-[20px] px-[35px] w-[180px] rounded-[5px]'>{children}</div>
+  )
+}
+
+export default OutLineBtn
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
 import Header from '../../components/header/Header'
+import OutLineBtn from '../../components/buttons/OutLineBtn/OutLineBtn'
 
 
 function Home() {
   return (
-    <Header />
+    <>
+      <Header />
+      <main>
+        <section className='Container pt-[70px]'>
+          <div>
+            <h4>Категории</h4>
+            <h4>Товаров</h4>
+          </div>
+          <OutLineBtn>Все товары</OutLineBtn>
+        </section>
+      </main>
+
+    </>
   )
 }
 
