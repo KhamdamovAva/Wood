a32dfc7dcd9f86c054e6e363799904ce1fa5044e
Beksulton10 <beksulton460@gmail.com>
2024-11-27 16:07:52 +0500

    I added to OutLineBtn icon arrow_icon.svg

--- src/components/buttons/OutLineBtn/OutLineBtn.jsx
+++ src/components/buttons/OutLineBtn/OutLineBtn.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import arrow from '../../../assets/icons/arrow_icon.svg'
 
 function OutLineBtn({ children }) {
   return (
-    <div className='border-2 border-[#D8A47A] py-[20px] px-[35px] w-[180px] rounded-[5px]'>{children}</div>
+    <button className='border-2 border-[#D8A47A] py-[15px] px-[25px] w-[170px] h-[60px] rounded-[5px] flex items-center text-center'>{children} <img src={arrow} alt="icon" className='pt-[2px] pl-[25px]'/></button>
   )
 }
 
