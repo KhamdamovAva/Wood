0bd5788485f3d8a4daafc02de72897803dfa3059
KhamdamovAva <73744057+Number1ava@users.noreply.github.com>
2024-11-27 15:34:02 +0500

    test

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
-
+import ambarIcon from '../../assets/icons/ambarIcon.svg'
 function Navbar() {
   return (
-    <div>Navbar</div>
+    <nav className='flex items-center justify-between text-[14px] font-medium'>
+      <img src={ambarIcon} alt="Logotype" />
+      <ul className='flex space-x-[44px]'>
+        <li><a href="#">Каталог</a></li>
+        <li><a href="#">Об амбарной доске</a></li>
+        <li><a href="#">Наши работы</a></li>
+        <li><a href="#">О нас</a></li>
+        <li><a href="#">Блог</a></li>
+        <li><a href="#">Контакты</a></li>
+      </ul>
+      <div className='flex '>
+        <a href="#" className='w-[172px] text-[12px] block'>+7 (929) 590 82-87<br></br> Москва и Санкт-Петербург</a>
+        <button>Консультация</button>
+      </div>
+    </nav>
   )
 }
 
