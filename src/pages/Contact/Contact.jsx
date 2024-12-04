d867aab335c314e928fb02d6eed1c1630fa3cbc0
KhamdamovAva <73744057+Number1ava@users.noreply.github.com>
2024-12-04 14:47:07 +0500

    added style background

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -4,32 +4,37 @@ import Location from '../../components/Location/Location'
 import Form from '../../components/form/Form'
 import formImage from "../../assets/images/formImage.png";
 import Footer from '../../components/footer/Footer';
+import contactBG from '../../assets/images/contactBG.png'
+import Navbar from '../../components/navbar/Navbar';
 
 function Contact() {
-  return (
-    <>
-       <header>
-          <div className="headerContainer">
-             <HeaderSecond />
-          </div>
-       </header>
-       <main>
-          <section>
-             <div className="Container">
-                <Location />
-             </div>
-          </section>
-          <section>
-             <div style={{backgroundImage : `url(${formImage})`}} className='mt-[60px]'>
-                <Form  title="Остались" subtitle="вопросы ?" bid="Оставьте заявку или позвоните нам по номеру:" phone="+7 (929) 590 82-87"/>
-             </div>
-          </section>
-       </main>
-       <footer>
-           <Footer />
-       </footer>
-    </>
-  )
+   return (
+      <>
+         <header>
+            <div className="headerContainer">
+               <Navbar />
+               <div className='bg-cover bg-no-repeat' style={{ backgroundImage: `url(${contactBG})` }}>
+                  <HeaderSecond title="Контакты"/>
+               </div>
+            </div>
+         </header>
+         <main>
+            <section>
+               <div className="Container">
+                  <Location />
+               </div>
+            </section>
+            <section>
+               <div style={{ backgroundImage: `url(${formImage})` }} className='mt-[60px]'>
+                  <Form title="Остались" subtitle="вопросы ?" bid="Оставьте заявку или позвоните нам по номеру:" phone="+7 (929) 590 82-87" />
+               </div>
+            </section>
+         </main>
+         <footer>
+            <Footer />
+         </footer>
+      </>
+   )
 }
 
 export default Contact
