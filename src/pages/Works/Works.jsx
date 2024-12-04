e14de348b4a4b6459d0701f5d3655524376c4599
Beksulton10 <beksulton460@gmail.com>
2024-12-04 12:47:35 +0500

    I finished this section filter

--- src/pages/Works/Works.jsx
+++ src/pages/Works/Works.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import HeaderSecond from '../../components/headerSecond/HeaderSecond'
+import { works, cards } from '../../Data/worksData'
 
 function Works() {
   return (
@@ -11,6 +12,41 @@ function Works() {
       </header>
       <main>
         <section>
+          <div className="Container">
+            <p className='text-[20px] font-medium text-[#C38F65]'>ФИЛЬТРЫ:</p>
+            <div className='flex mt-[14px]'>
+              {works.map((item) => (
+                <div key={item.id} className='items-center flex text-[16px] font-medium'>
+                  <button className='border border-[#D8A47A] py-[12px] px-[28px]  h-[74px] rounded-[5px] mr-[28px]'>{item.btn}</button>
+                </div>
+              ))}
+            </div>
+            <div className='grid grid-cols-3 mt-[40px]'>
+              {cards.map((item) => (
+                <div
+                  key={item.id}
+                  style={{
+                    backgroundImage: `url(${item.img})`,
+                    backgroundSize: 'cover',
+                    backgroundPosition: 'center',
+                  }}
+                  className="h-[377px] w-[380px] py-[32px] px-[37px] mb-[20px] relative"
+                >
+                  {/* Полупрозрачный чёрный слой */}
+                  <div className="absolute inset-0 bg-black opacity-20 rounded-[30px]"></div>
+
+                  {/* Содержимое карточки */}
+                  <div className='relative mt-[240px] h-[70px]'>
+                    <p className='text-white text-[20px] mb-[15px]'>{item.title}</p>
+                    <button href="#" className="cursor-pointer text-[#CF9B74] border-b border-[#CF9B74] mt-auto">
+                      Подробнее
+                    </button>
+                  </div>
+                </div>
+              ))}
+            </div>
+
+          </div>
         </section>
       </main>
     </>
